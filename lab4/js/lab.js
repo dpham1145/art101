2598465dbfad7831b35e8058bda2a29c438cb157
dpham15 <phamdamon@gmail.com>
2023-10-22 22:30:39 -0700

    Lab 4 submit

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//Connect 4
+//An implementation of the game
+//Damon Pham <dpham15@ucsc.edu>
+//Octover 2023
+
+
+// Print a 6x7  slot board
+// Accept user input for each column
+  // Validate user move (if they are able to commit the move)
+  // Move player’s input piece to lowest empty space in the column
+  // Print board with player's move added
+// Evaluate possible moves for computer
+  // Loop through each column to determine the best valid move
+  // Choose computer move
+  // Move computer’s piece to lowest empty space in the column  
+  // Print board with computer's move added 
+// Confirm the winner when 4 pieces make a row, column, or diagonal
 
 // Constants
 
