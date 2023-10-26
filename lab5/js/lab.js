6796a61926762014691c5c6d4c4a169e016121f3
dpham15 <phamdamon@gmail.com>
2023-10-26 08:34:09 -0700

    Turn in Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Damon Pham
+// Date: 10/24/2023
 
-// Constants
+//Defining Variables
+make = "honda";
+model = "accord";
+color = "white";
+year = 2020;
+ownIt = true;
 
-// Functions
+//calculate
+age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Own It: " + ownIt + "<br>");
+document.writeln("Age: " + age + " years<br>");
