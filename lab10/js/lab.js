914d3107a34c91b39a017727c11be6a4a728723f
dpham15 <phamdamon@gmail.com>
2023-11-14 08:43:43 -0800

    Implement Lab 10 and 11

---
+++ lab10/js/lab.js
@@ -0,0 +1,11 @@
+/*lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page.
+
+Requirements: jQuery must be loaded for this script to work.
+
+Author: Damon Pham
+Date: 11/14/2023
+*/
+
+//add button to challenge section
+
+
---
+++ lab11/js/lab.js
@@ -0,0 +1,10 @@
+/*lab.js - This simple JavaScript/jQuery script uses buttons to modify some elements on the page.
+
+Requirements: jQuery must be loaded for this script to work.
+
+Author: Damon Pham
+Date: 11/12/2023
+*/
+
+
+
