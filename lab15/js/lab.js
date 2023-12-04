87747731a83fe28c55a505a6a640c62638e97a88
dpham15 <phamdamon@gmail.com>
2023-12-03 21:13:31 -0800

    Turn in 15

---
+++ lab15/js/lab.js
@@ -0,0 +1,29 @@
+/*lab.js - This simple JavaScript/jQuery script gets values from other sites and outputs them on local site.
+
+Requirements: jQuery must be loaded for this script to work.
+
+Author: Damon Pham
+Date: 12/3/2023
+*/
+
+const ENDPOINT = "https://yesno.wtf/api/";
+
+//attach an event listener
+$("#activate").click(function(){
+  //ajax this
+  $.ajax({
+    "url": ENDPOINT,
+    "dataType": "json",
+    "success": function(results){
+      console.log(results);
+      //in the callback:
+      //extract the results
+      jokeText = results.answer;
+      //add to output div
+      $("#output").append("<p>" + jokeText);
+    },
+    "error": function(){
+
+    }
+  })
+})
